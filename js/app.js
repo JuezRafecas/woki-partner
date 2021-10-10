df668797a8daf9b24c5de54715ae70a36ac2227f
JuezRafecas <gijonrafaelantonio@gmail.com>
2021-10-10 12:32:09 -0300

    modificacion url api sheets

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@ let botonForm = document.querySelector('.send');
 botonForm.addEventListener('click', SubForm());
 function SubForm (){
     $.ajax({
-        url:'https://api.apispreadsheets.com/data/19231/',
+        url:'https://api.apispreadsheets.com/data/19248/',
         type:'post',
         data:$("#formulario").serializeArray(),
         success: function(){
-          console.log('Datos cargados en excel');
+            console.log('Datos cargados en excel');
         },
         error: function(){
             console.log('Fallo en la carga de datos');
