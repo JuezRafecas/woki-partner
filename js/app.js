4b90d889c150ff5159f278c4aa8303fa8aaf033b
JuezRafecas <gijonrafaelantonio@gmail.com>
2021-10-11 15:04:08 -0300

    sticky header dinamico

--- js/app.js
+++ js/app.js
@@ -1,13 +1,36 @@
-function SubForm (){
-    $.ajax({
-        url:'https://api.apispreadsheets.com/data/19248/',
-        type:'post',
-        data:$("#formulario").serializeArray(),
-        success: function(){
-            console.log('Datos cargados en excel');
-        },
-        error: function(){
-            console.log('Fallo en la carga de datos');
-        }
+window.addEventListener('load', function(){
+    const body = document.querySelector('header');
+    const scrollUp = "scroll-up";
+    const scrollDown = "scroll-down";
+    let lastScroll = 0;
+    window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll == 0) {
+        body.classList.remove(scrollUp);
+        return;
+    }
+    if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+        // down
+        body.classList.remove(scrollUp);
+        body.classList.add(scrollDown);
+    } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+        // up
+        body.classList.remove(scrollDown);
+        body.classList.add(scrollUp);
+    }
+    lastScroll = currentScroll;
     });
-}
+    function SubForm (){
+        $.ajax({
+            url:'https://api.apispreadsheets.com/data/19248/',
+            type:'post',
+            data:$("#formulario").serializeArray(),
+            success: function(){
+                console.log('Datos cargados en excel');
+            },
+            error: function(){
+                console.log('Fallo en la carga de datos');
+            }
+        });
+    }
+})
